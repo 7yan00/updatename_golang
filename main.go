49f35bd65d13d33553f514130e6e9afbcccdc91e
ryusen33 <ryusen33sasa@gmail.com>
2014-11-06 22:43:06 +0900

    fix syntax

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ import (
 )
 
 type user struct {
-	ID         int64  `json:"id"`
+	Id         int64  `json:"id"`
 	Name       string `json:"name"`
 	ScreenName string `json:"screen_name"`
 }
 type status struct {
-	ID   int64  `json:"id"`
+	Id   uint64 `json:"id"`
 	Text string `json:"text"`
 	User user   `json:"user"`
 }
@@ -37,8 +37,8 @@ var consumerSecret *string = flag.String(
 var accessToken *oauth.AccessToken
 
 func main() {
-	screenName := "rrr"
-	re := regexp.MustCompile(`^(.+)\(@` + screenName + `\)$`)
+	ScreenName := "ryusen33"
+	re := regexp.MustCompile(`^(.+)\(@` + ScreenName + `\)$`)
 	flag.Parse()
 	fmt.Println("loading consumerkey......")
 	loading()
@@ -50,8 +50,8 @@ func main() {
 			return
 		}
 
-		match := re.FindStringSubmatch(s.text)
-		if len(match) != nil {
+		match := re.FindStringSubmatch(s.Text)
+		if len(match) != 2 {
 			return
 		}
 
@@ -65,7 +65,7 @@ func main() {
 		}
 
 		fmt.Println(newName)
-		err = UpdateStatus(fmt.Sprintf("@%v 「%v」に改名したのです", s, user.screenName, newName), s.Id)
+		err = UpdateStatus(fmt.Sprintf("@%v 「%v」に改名したのです", s, s.User.ScreenName, newName), s.Id)
 
 		if err != nil {
 			fmt.Println("tweet failed")
@@ -139,7 +139,7 @@ func get_timeline(procLine func(b []byte)) {
 
 func UpdateStatus(text string, inReplyToStatusId uint64) error {
 	response, err := c.Post("https://api.twitter.com/1.1/statuses/update.json",
-		map[string]string{"status": string{text}, "in_reply_to_status_id": string{fmt.Sprint(inReplyToStatusId)}}, accessToken)
+		map[string]string{"status": text, "in_reply_to_status_id": fmt.Sprint(inReplyToStatusId)}, accessToken)
 
 	if err != nil {
 		return err
